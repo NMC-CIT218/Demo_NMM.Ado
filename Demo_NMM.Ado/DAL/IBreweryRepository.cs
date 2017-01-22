b77cb36828fec845c3dd084dff353e9a919a88bc
John Velis <john@johnvelis.com>
2017-01-22 08:57:09 -0500

    commit857

---
+++ Demo_NMM.Ado/DAL/IBreweryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Demo_NMM.Ado.Models;
+
+namespace Demo_NMM.Ado.Dal
+{
+    public interface IBreweryRepository
+    {
+        void Insert(Brewery brewery);
+        void Update(Brewery brewery);
+        void Delete(int id);
+        Brewery SelectOne(int id);
+        IEnumerable<Brewery> SelectAll();
+    }
---
+++ Demo_NMM.Ado/Models/Brewery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo_NMM.Ado.Models
+{
+    public class Brewery
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        public string URL { get; set; }
+    }
+}
